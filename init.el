31e6eb982504e432d4ebe53912b1f7d1e7ce8870
Jeff Rush <jrush@zeomega.org>
2022-01-19 03:46:38 -0600

    Cleaned up config.org and the .emacs.d/ directory.

--- init.el
+++ init.el
@@ -1,83 +1,107 @@
 ; -*-mode: Emacs-Lisp; auto-recompile:nil; outline-minor-mode:t-*-
 
-;;;;;;
-;;; How My Emacs Configuration Fits into the Emacs Startup Sequence
-                                        ;
-;;; When Emacs is started, it tries to load a ELisp program from an init file,
-;;; looking in the following places in this order:
-;;;
-;;;    - ~/.emacs              -- I don't use this file
-;;;    - ~/.emacs.el           -- I don't use this file
-;;;    - ~/.emacs.d/init.el    -- This is the *start of my Emacs configuration*
-;;;
-;;; In case of problems with init.el, you can use the command-line switch '-q'
-;;; to prevent it being loaded or '--debug-init' to get a traceback of the
-;;; first error encountered.
-;;;
-;;; TIP: It is NOT recommendeed to byte-compile your init file as it does not
-;;;      speed up startup very much and often leads to problems when you forget
-;;;      to recompile the file.
-
-;;; From the init.el file, I enable the Cask package manager and the Org-Mode
-;;; library and the rest of my configuration is specified in an .org file
-;;; where I can more easily make notes about settings and organize the various
-;;; fragments.
-
-;;; My Directory Structure:
-;;
-;;   ~/.emacs.d/
-;;      README.org		;; light introduction to my setup
-;;      ARCHIVE/		;; modules that I retired using and will someday delete
-;;      backups/		;; backup copies of ANY edited files
-;;      init.el			;; my init file for Emacs (versus ~/.emacs)
-;;      config.org		;; my Emacs configuration in org-mode 'literate' format
-;;      config.el		;; auto-built from my config.org each time Emacs starts
-;;      custom-settings.el	;; tiny custom settings auto-edited by Emacs itself
-;       Cask			;; installed modules list read by Cask and maintained by Pallet
-;;      .cask/
-;;         25.3/		;; cache/collection of modules pulled down by package manager
-;;      snippets/		;; my library of text fragments for use by the snippet library
-;;      lisp/			;; individual .el files I create or work on
-;;      ac-dict/		;; auto-complete dictionary for various modes/languages
-;;
-;;   ~/.cask/
-;;      Cask			;; specification file of dependencies of the Cask pgm
-;;      bin/
-;;         cask			;; command-line tool for invoking Cask outside of Emacs
-;;      cask.el			;; ELisp source of the Cask library
+;;; I put a minimal set of configurations here in the init.el file, and
+;;; the rest into my config.org file where I can more easily make nodes
+;;; abouto settings and organize the various fragments of emacs-lisp.
 
 ; **********************************************************************
 ;   Define Various Emacs Policies re Debugging and Analysis of Issues
 ; **********************************************************************
 
 ; Useful for debugging problems with Emacs on occasion.
-;;; (setq debug-on-error t)
+(setq debug-on-error t)
 (setq stack-trace-on-error t)
-; (debug-on-entry 'integerp)
 
-; Get 'package' set up with paths to public repositories
-(require 'package)
+; **********************************************************************
+;     List Package Archives From Which I May Pull Add-Ons to Emacs
+; **********************************************************************
+
+(require 'package)  ;; the Emacs package manager
+
 (setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-  '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives
-  '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives
-  '("gnu" . "http://elpa.gnu.org/packages/"))
 
-;; This sets up the load path so that we can override it next.
-(package-initialize)
+;; For security reasons, use "https" on all archive URLs.  And I
+;; want to try turning on a requirement that all packages are signed
+;; but haven't yet.
+
+(add-to-list 'package-archives
+  '("melpa" . "https://melpa.org/packages/"))
+
+(add-to-list 'package-archives
+  '("melpa-stable" . "https://stable.melpa.org/packages/"))
+
+(setq package-archive-priorities ;; define the order of preference
+  '(("melpa-stable" . 20)  ;; most preferred
+    ("gnu" . 10)
+    ("melpa" . 0))         ;; least preferred
+)
 
 ;; Define where various packages I wish to use, that come in
 ;; form of Git checkouts, or that I've written, is kept.
-(add-to-list 'load-path "~/.emacs.d/lisp/")
+
+(add-to-list 'load-path
+  "~/.emacs.d/lisp/")
+
+;; Find all installed packages along the load-path and *activate* them by
+;; executing their <package>-autoloads.el files.  It tries to make sure to
+;; activate only the *latest* version of a package in case there are several
+;; versions found.
+
+(package-initialize)
+
+(when (not package-archive-contents)   ;; if cache of 'package-archives' sites is empty,
+  (package-refresh-contents))          ;;   then download latest package descriptions
 
 ; **********************************************************************
-;  Minimum Setup Necessary Before Loading Configuration from .org File
+;                   Bootstrap the "quelpa" package
+;
+; Quelpa is a tool for building and installing Emacs Lisp packages
+; on-the-fly and directly from source.  I use it only to install packages
+; NOT in one of the above package archives, to install those things that
+; exist only in less well known Git repositories or the odd .el file.
+;
+; https://github.com/quelpa/quelpa
 ; **********************************************************************
 
-(require 'cask "~/.cask/cask.el")
-(cask-initialize)
+(unless (package-installed-p 'quelpa)  ;; install if NOT already installed
+  (with-temp-buffer
+    (url-insert-file-contents "https://github.com/quelpa/quelpa/raw/master/quelpa.el")
+    (eval-buffer)
+    (quelpa-self-upgrade)
+  )
+)
 
-(require 'org)
+(setq quelpa-checkout-melpa-p
+  nil) ;; disable quelpa peeking into MELPA and installing things from source
+
+; **********************************************************************
+;                Bootstrap the "use-package" macro
+;
+; The "use-package" macro provides a tidy, performance-oriented way to
+; isolate the configuration and loading of packages within my Emacs
+; configuration.  "use-package" is NOT a package manager itself but it
+; interfaces with package managers to work.
+; **********************************************************************
+
+(quelpa ;; install the quelpa extension to use-package
+  '(quelpa-use-package
+     :fetcher git
+     :url "https://github.com/quelpa/quelpa-use-package.git"
+   )
+)
+(require 'quelpa-use-package)
+
+;; install the "use-package-ensure" which pulls in the rest of "use-package",
+;; and then default the :ensure flag to always require a package if it is
+;; referenced by "use-package".
+
+(require 'use-package-ensure)
+(setq use-package-always-ensure t)
+
+; **********************************************************************
+;      Load the Rest of My Configuration from My config.org File
+; **********************************************************************
+
+(use-package org)
 (org-babel-load-file "~/.emacs.d/config.org" nil)  ;; Tangle into .el and load
+(put 'upcase-region 'disabled nil)
