e4cd8b6d67b661273d9c829de0578f63d9db85d4
Jeff Rush <jrush@zeomega.org>
2023-11-28 02:36:38 -0600

    Finished draft of early-init.el for publishing.

--- early-init.el
+++ early-init.el
@@ -2,110 +2,121 @@
 
 ;; Author: Jeffrey Rush
 ;; Keywords: Emacs configuration
-;; Homepage: https://github.com/xanalogica/.emacs.d.git
+;; Homepage: https://github.com/xanalogica/emacs.d.git
 
 ;;; Commentary:
+
+;; This early-init.el file was introduced in Emacs 27.1 and is loaded BEFORE
+;; the package system and GUI is initialized.  This is a great place for
+;; *startup optimizing*, because only here can you *prevent* things from
+;; loading, rather than turn them off after-the-fact.
+
+;; WARNING: It is NOT recommended that you move into early-init.el those
+;; customizations that can be left in the normal init files, because
+;; early-init.el is read before the GUI is initialized, so customizations
+;; related to GUI features *will not work reliably* in early-init.el.
+
 ;; Emacs 29.1+ configuration.
 
 ;;; Code:
 
-;;;; configure emacs internal processes
+(cond ((version< emacs-version "29.1")
+       (warn "My Emacs configuration requires Emacs 29.1 and above!"))
+)
 
-; **********************************************************************
-;   Define Various Emacs Policies re Debugging and Analysis of Issues
-; **********************************************************************
+;; **********************************************************************
+;;   Define Various Emacs Policies re Debugging and Analysis of Issues
+;; **********************************************************************
 
-; Useful for debugging problems with Emacs on occasion.
-(setq debug-on-error t)
-(setq stack-trace-on-error t)
+;; Useful for debugging problems with Emacs on occasion.
+; (setq debug-on-error t)
+; (setq stack-trace-on-error t)
 ; (debug-on-entry 'integerp)
 
-;; this makes garbage collection less frequent, which speeds up init by about 2 seconds.
-(setq gc-cons-threshold 80000000)     ;; JEFF
-;;(setq gc-cons-threshold 800000)  ;; original value
+;; Respect DEBUG envvar as an alternative to --debug-init, and
+;; to make are startup sufficiently verbose from this point on.
+(when (getenv-internal "DEBUG")
+  (setq init-file-debug t
+        debug-on-error t))
+
+;; **********************************************************************
+;;                 Define Garbage Collection Policies
+;;
+;; Garbage collection is a big contributor to startup times.  This fends
+;; it off, but will be reset later by `gcmh-mode'.  Not resetting it later
+;; will cause stuttering/freezes.
+;;
+;; **********************************************************************
 
 (setq
-  gc-cons-threshold               most-positive-fixnum
-  read-process-output-max         (* 1024 1024 4) ; 4mb
-  inhibit-compacting-font-caches  t
-  inhibit-startup-message         t
-  message-log-max                 16384
-  package-enable-at-startup       nil
-  load-prefer-newer               noninteractive)
+      ;; Defer garbage collection further back in the startup process, until
+      ;; reset later by `gcmh-mode'.  NOT resetting it later will cause
+      ;; stuttering and freezing during long-term interactive use.
 
-;;;; gc
+      gc-cons-threshold                  most-positive-fixnum
 
-;;;; default frame
+      ;; Portion of the heap used for allocation.
+      ;;
+      ;; Garbage collection can happen automatically once this portion of the
+      ;; heap has been given out since the last garbage collection.
+      ;;
+      ;; By binding this temporarily to a large number, you can effectively
+      ;; prevent garbage collection during a part of the program.  But be sure
+      ;; to get back to the normal value soon enough, to avoid system-wide
+      ;; memory pressure, and never use a too-high value for prolonged periods
+      ;; of time.
 
-(setq-default
-  default-frame-alist '((width . 170)
-                        (height . 56)
-                        (tool-bar-lines . 0)
-                        (bottom-divider-width . 0)
-                        (right-divider-width . 1))
+      gc-cons-percentage                 0.6
 
-  initial-frame-alist           default-frame-alist
-  frame-inhibit-implied-resize  t
-  x-gtk-resize-child-frames     'resize-mode
-  fringe-indicator-alist        (assq-delete-all 'truncation fringe-indicator-alist))
+      ;; In noninteractive sessions only, prioritize non-byte-compiled source
+      ;; files to prevent the use of stale byte-code.  Otherwise, it saves us
+      ;; a little I/O time to skip the mtime checks on every *.elc file.
 
-;;;  (setq default-frame-alist                           JEFF
-;;;    '(
-;;;  ;--      (top              . 200) ; only for GUI
-;;;  ;--      (left             . 400)
-;;;  ;--      (width            . 80)
-;;;  ;--      (height           . 40)
-;;;;;NEEDED?        (cursor-color     . "white")
-;;;;;NEEDED?        (cursor-type      . box)
-;;;;;NEEDED?        (foreground-color . "black")
-;;;;;NEEDED?        (background-color . "white")
-;;;        )
-;;;      )
+      load-prefer-newer                  'noninteractive
+)
 
+(setq
+      site-run-file                      nil
 
+      ;; Maximum number of bytes to read from subprocess in a single chunk.
+      ;;
+      ;; Enlarge the value only if the subprocess generates very large (megabytes)
+      ;; amounts of data in one go.
 
+      read-process-output-max            (* 1024 1024 4) ; 4 MB
 
+      ;; Do NOT compact (discard) cached fonts during garbage collection.
+      ;;
+      ;; Some large fonts cause lots of consing and trigger GC.  If they are
+      ;; discarded from the font caches during garbage collection, they will need
+      ;; to be opened AGAIN during redisplay, which slows down redisplay.
+      ;;
+      ;; If you see font-related delays in displaying some special characters, and
+      ;; cannot switch to a smaller font for those characters, set this variable
+      ;; non-nil.  Disabling compaction of font caches might enlarge the Emacs
+      ;; memory footprint in sessions that use lots of different fonts.
 
-;;; (unless (or (daemonp) noninteractive)
-;;;   (let ((restore-file-name-handler-alist file-name-handler-alist))
-;;;     (setq-default file-name-handler-alist nil)
-;;;     (defun restore-file-handler-alist ()
-;;;       (setq file-name-handler-alist
-;;;             (delete-dups (append file-name-handler-alist
-;;;                                  restore-file-name-handler-alist)))))
+      inhibit-compacting-font-caches     t
+)
 
+;; **********************************************************************
+;;    Declare Package Archives From Which I May Pull Add-Ons to Emacs
+;; **********************************************************************
 
-;;;   (add-hook 'emacs-startup-hook #'restore-file-handler-alist 101)
+(setq
 
-  ;;;;;;;;;; (when (fboundp #'tool-bar-mode)
-  ;;;;;;;;;;   (tool-bar-mode -1))
+      ;; Package initialize occurs automatically, BEFORE user-init-file is
+      ;; loaded, but AFTER early-init-file.  We handle package initialization,
+      ;; so we must prevent Emacs from doing it early and let us do it!
 
-  ;;;;;;;;;; (when (fboundp #'scroll-bar-mode)
-  ;;;;;;;;;;   (scroll-bar-mode -1)))
+      package-enable-at-startup          nil
 
-(when (featurep 'native-compile)
-  (defvar inhibit-automatic-native-compilation)
-  (setq   inhibit-automatic-native-compilation nil)
-  (defvar native-comp-async-report-warnings-errors)
-  (setq   native-comp-async-report-warnings-errors 'silent))
+      ;; `use-package' is built into Emacs since version 29 but not loaded
+      ;; until we say so.  So before we load `use-package', we can enable imenu
+      ;; support so it will see `use-package' declarations.
 
-;;; (defun edit-init-file ()
-;;;   "Edit `user-init-file'.
-;;; With prefix argument prompts to select a file from all Emacs Lisp
-;;; in `user-emacs-directory'."
-;;;   (interactive)
-;;;   (if current-prefix-arg
-;;;       (find-file
-;;;        (expand-file-name
-;;;         (completing-read
-;;;         "file"
-;;;          (directory-files user-emacs-directory nil "^[^.].*.el$"))
-;;;         user-emacs-directory))
-;;;     (find-file (expand-file-name "init.el" user-emacs-directory))))
-
-; **********************************************************************
-;    Declare Package Archives From Which I May Pull Add-Ons to Emacs
-; **********************************************************************
+      use-package-enable-imenu-support   t
+)
 
 (require 'package)  ;; the Emacs package manager
 
@@ -126,9 +137,9 @@
 )
 
 ;; Define where various packages I wish to use, that come in
-;; form of Git checkouts, or that I've written, is kept.
+;; form of Git checkouts, or that I've written, are kept.
 
-;;;;;;;;;; (add-to-list 'load-path "~/.emacs.d/lisp/")
+;;;;; (add-to-list 'load-path "~/.emacs.d/lisp/")
 
 ;; Find all installed packages along the load-path and *activate* them by
 ;; executing their <package>-autoloads.el files.  It tries to make sure to
@@ -141,7 +152,7 @@
 ;;;;;;;;;;   (package-refresh-contents))          ;;   then download latest package descriptions
 
 ; **********************************************************************
-;                   Bootstrap the "quelpa" package
+;                Bootstrap the "quelpa" Package Manager
 ;
 ; Quelpa is a tool for building and installing Emacs Lisp packages on-the-fly
 ; and directly from source.  I use it only to install packages NOT in one of
@@ -160,20 +171,20 @@
 )
 
 (setq quelpa-checkout-melpa-p
-  nil) ;; disable quelpa peeking into MELPA and installing things from source
+  nil)  ;; disable quelpa peeking into MELPA and installing things from source
 
 ; **********************************************************************
 ;                Bootstrap the "use-package" macro
 ;
-; The "use-package" macro provides a tidy, performance-oriented way to
-; isolate the configuration and loading of packages within my Emacs
-; configuration.  "use-package" is NOT a package manager itself but it
-; interfaces with package managers to work.
+; The `use-package' macro provides a tidy, performance-oriented way to isolate
+; the configuration and loading of packages within my Emacs configuration.
+; `use-package' is NOT a package manager itself but it interfaces with package
+; managers to work.
 ; **********************************************************************
 
 ; adds support for the :quelpa keyword in use-package clauses.
 
-(quelpa ;; install the quelpa extension to use-package
+(quelpa  ;; install the `quelpa' extension to `use-package'
   '(quelpa-use-package
      :fetcher git
      :url "https://github.com/quelpa/quelpa-use-package.git"
@@ -188,18 +199,105 @@
 ;;;;;;;;;; (require 'use-package-ensure)
 ;;;;;;;;;; (setq use-package-always-ensure t)
 
-;;;;;;;;;; ???  (require 'cask "~/.cask/cask.el")
-;;;;;;;;;; ???  (cask-initialize)
+; **********************************************************************
+;                  Compile .el into .elc Bytecode
+; **********************************************************************
+
+; native-comp-deferred-compilation
+(setq native-comp-jit-compilation nil)
+
+(when (featurep 'native-compile)
+  (defvar inhibit-automatic-native-compilation)
+  (setq   inhibit-automatic-native-compilation nil)
+  (defvar native-comp-async-report-warnings-errors)
+  (setq   native-comp-async-report-warnings-errors 'silent))
+
+; **********************************************************************
+;      Prevent Some Brief UI Annoyances that Flash on the Screen
+; **********************************************************************
+
+;; Prevent the glimpse of un-styled Emacs by disabling these UI elements early.
+;;
+;; Disable unnecessary interfaces
+;;
+;; It will be faster to disable them here before they've been initialized.
+
+;;;;; (setq-default
+;;;;;   x-gtk-resize-child-frames     'resize-mode
+;;;;;   fringe-indicator-alist        (assq-delete-all 'truncation fringe-indicator-alist)
+;;;;; )
+
+(setq
+
+      ;; Avoid displaying the start screen.
+
+      inhibit-startup-message            t
+
+      ;; Disable built-in mode-line because we have own config
+
+      mode-line-format                   nil
+
+      ;; ???
+
+      frame-resize-pixelwise             t
+
+      ;; Resizing the Emacs frame can be a terribly expensive part of changing
+      ;; the font.  By inhibiting this, we easily halve startup times with
+      ;; fonts that are larger than the system default.
+
+      frame-inhibit-implied-resize       t
+
+      default-frame-alist '(
+        (fullscreen . maximized)
+
+        ;; You can turn off scroll bars by uncommenting these lines:
+        ;; (vertical-scroll-bars . nil)
+        ;; (horizontal-scroll-bars . nil)
+
+        (menu-bar-lines . 0)
+        (tool-bar-lines . 0)
+        (vertical-scroll-bars . nil)
+
+        (bottom-divider-width . 0)
+        (right-divider-width . 1)
+        ;; Setting the face in here prevents
+        ;; flashes of color as the theme gets activated
+        (background-color . "#000000")
+        (ns-appearance . dark)
+        (ns-transparent-titlebar . t)
+    )
+    ;;; initial-frame-alist           default-frame-alist
+)
+
+(tool-bar-mode -1)  ; All these tools are in the menu-bar anyway
+(menu-bar-mode -1)
+
+; **********************************************************************
+;      Message Log Buffer
+; **********************************************************************
+
+(setq
+
+      ;; Maximum number of lines to keep in the message log buffer.
+
+      message-log-max                    16384
+)
 
 
+;; annoyance supression
+(setq byte-compile-warnings '(not obsolete))
+(setq warning-suppress-log-types '((comp) (bytecomp)))
+(setq native-comp-async-report-warnings-errors 'silent)
 
+;; Silence stupid startup message
+(setq inhibit-startup-echo-area-message (user-login-name))
 
-
-
+;; Ignore X resources; its settings would be redundant with the other settings
+;; in this file and can conflict with later config (particularly where the
+;; cursor color is concerned).
+(advice-add #'x-apply-session-resources :override #'ignore)
 
 (message "early-init.el finishing up")
-
-
 
 (provide 'early-init)
 ;;; early-init.el ends here
